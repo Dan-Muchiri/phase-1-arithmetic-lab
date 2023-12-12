1f54d42345ce785d44a3fed255a5ae81f1df88d6
Dan Muchiri <dan.muchiri@student.moringaschool.com>
2023-12-12 14:23:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
+const random = Math.ceil(Math.random() * 10) + 1;
+let num3 = 22;
+let num4 = 9;
+const mod = num3 % num4;
+let max;
+ max = Math.max(10,13,12,7,20);
